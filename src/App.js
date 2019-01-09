5e7744f617fff88ed313ace56c771eb316ed5176
ashalkey <alanshalkey@gmail.com>
2019-01-09 16:51:35 -0700

    creating components, setting up axios get request in getWeather method

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+import axios from "axios";
+
+const API_KEY = "2e1c5a3fbaca4a3d9d14564ae28c29da";
 
 class App extends Component {
+
+  getWeather = () => {
+    axios.get(`api.openweathermap.org/data/2.5/forecast`, {
+      params: {
+        q: "Manchester,uk",
+        APPID: API_KEY
+      }
+    })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error){
+      console.log(error);
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather}/>
+        <Weather />
       </div>
     );
-  }
-}
+  }  
+};
 
 export default App;
