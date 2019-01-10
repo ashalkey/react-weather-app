fa7593cc48de4bf5ee55be82d24448e02cbc1c9e
ashalkey <alanshalkey@gmail.com>
2019-01-10 11:06:37 -0700

    adding formatting for api data, conditional rendering, next to add: styling

--- src/App.js
+++ src/App.js
@@ -8,27 +8,58 @@ const API_KEY = "2e1c5a3fbaca4a3d9d14564ae28c29da";
 
 class App extends Component {
 
-  getWeather = () => {
-    axios.get(`api.openweathermap.org/data/2.5/forecast`, {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    error: undefined,
+    description: undefined,
+  }
+
+  convertKelvinToFarenheit = (k) => {
+    return Number.parseFloat((k - 273.15) * (9/5) + 32).toFixed(2);
+  }
+
+  getWeather = (e) => {
+    e.preventDefault();
+    let city = e.target.elements.city.value;
+    let country = e.target.elements.country.value;
+    axios.get(`https://api.openweathermap.org/data/2.5/forecast`, {
       params: {
-        q: "Manchester,uk",
+        q: `${city},${country}`,
         APPID: API_KEY
       }
     })
-    .then(function(response) {
+    .then((response) => {
       console.log(response);
+      this.setState({
+        temperature: `${this.convertKelvinToFarenheit(response.data.list[0].main.temp)} °F`,
+        city: response.data.city.name,
+        country: response.data.city.country,
+        humidity: response.data.list[0].main.humidity,
+        description: response.data.list[0].weather[0].description,
+        error: ""
+      });
     })
     .catch(function(error){
       console.log(error);
     });
-  }
+  };
 
   render() {
     return (
       <div>
         <Titles />
         <Form getWeather={this.getWeather}/>
-        <Weather />
+        <Weather 
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }  
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,10 +2,10 @@ import React, {Component} from "react";
 
 class Form extends Component {
 
-    
+
     render() {
         return (
-        <form>
+        <form onSubmit={this.props.getWeather}>
             <input type="text" name="city" placeholder="City..."/>
             <input type="text" name="country" placeholder="Country..."/>
             <button>Get Weather</button>
